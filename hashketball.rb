b4577eac65f3907364e09a26ab1e1fb0ac7ca55e
Anthony Rubbo <anthony.rubbo@gmail.com>
2020-10-01 19:59:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -25,6 +25,16 @@ def game_hash
           slam_dunks: 7
         },
         "Brook Lopez" => {
+          number: 11,
+        shoe: 17,
+        points: 17,
+        rebounds: 19,
+        assists: 10,
+        steals: 3, 
+        blocks: 1,
+        slam_dunks: 15
+        },
+        "Mason Plumlee" => {
           
         }
       }
