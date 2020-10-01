c9350b1d1576ee3b5681a0a723f9c48b0708b6ff
Anthony Rubbo <anthony.rubbo@gmail.com>
2020-10-01 19:45:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -40,27 +40,27 @@ def game_hash
         },
       {
         "Mason Plumlee": {
-        number: 1,
-        shoe: 19,
-        points: 26,
-        rebounds: 11,
-        assists: 6,
-        steals: 3,
-        blocks: 8,
-        slam_dunks: 5 
-      },
+          number: 1,
+          shoe: 19,
+          points: 26,
+          rebounds: 11,
+          assists: 6,
+          steals: 3,
+          blocks: 8,
+          slam_dunks: 5 
+        },
       {
         "Jason Terry": {
-        number: 31,
-        shoe: 15,
-        points: 19,
-        rebounds: 2,
-        assists: 2,
-        steals: 4,
-        blocks: 11,
-        slam_dunks: 1 
-      }
-    },
+          number: 31,
+          shoe: 15,
+          points: 19,
+          rebounds: 2,
+          assists: 2,
+          steals: 4,
+          blocks: 11,
+          slam_dunks: 1 
+        }
+      },
     away: {
       team_name: "Charlotte Hornets",
       colors: ["Turquoise", "Purple"],
