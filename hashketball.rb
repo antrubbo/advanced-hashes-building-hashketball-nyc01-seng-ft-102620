4b184196a8f6b71fd4327506340f33aa4bec8924
Anthony Rubbo <anthony.rubbo@gmail.com>
2020-10-01 19:58:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,8 +5,16 @@ def game_hash
       colors: [],
       players: {
         "Alan Anderson" => {
-          
-        }
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 1
+        },
+        
       }
     },
     away: {
