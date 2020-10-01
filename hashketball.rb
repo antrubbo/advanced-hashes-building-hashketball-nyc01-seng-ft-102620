521b627ac5e659986ebe8df5617e95ad31bef2eb
Anthony Rubbo <anthony.rubbo@gmail.com>
2020-10-01 19:33:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -57,4 +57,10 @@ def game_hash
           slam_dunks: 1 
         }
       }
+    away: {
+      team_name: "Charlotte Hornets",
+      colors: ["Turquoise", "Purple"],
+      players:
+    }
+  }
 end
