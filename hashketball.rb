edda6b87ff9325c4f97768c8c6741e7cbd86c327
Anthony Rubbo <anthony.rubbo@gmail.com>
2020-10-01 19:39:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,9 +3,13 @@ require 'pry'
 def game_hash
   {
     home: {
-      
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "White"],
+      players: {}
     },
-    away: {}
+    away: {team_name: "Charlotte Hornets",
+      colors: ["Turquoise", "Purple"],
+      players: {}
   }
 end
 
